f5dc853a8658b51f59a24aa7df3c0cb927adefcf
faiwanttorest <ysb.project01@gmail.com>
2026-03-18 01:29:09 +0700

    build success

--- src/components/VenueCatalog.tsx
+++ src/components/VenueCatalog.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Card from "./Card";
-
+import { VenueJson, VenueItem } from "../../interface"; 
 export default async function VenueCatalog({venuesJson} : {venuesJson : Promise<VenueJson>}){
     const venues = await venuesJson
 
